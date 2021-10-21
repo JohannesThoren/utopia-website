4ddbc2ababd688d3131fc4ae4a6722aa94526280
johannes <johannes@lgjt.xyz>
2021-10-21 15:23:23 +0200

    added some components

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+      <div>
+            <Post :v-for="post in data" author="{{post[0]}}" title="{{post[1]}}" content="{{post[2]}}"/>
+      </div>
+      
+</template>
+
+<script>
+import Post from "./Post.vue"
+
+export default {
+      name: "PostList",
+      comments:  {
+            Post,
+      },
+      data() {
+            return {
+                  data: ""
+            }
+      }
+}
+</script>
+
+<style>
+
+</style>
